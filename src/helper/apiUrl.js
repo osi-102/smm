7053e5dd5585dc594c4436cd36a0b2e8bdb36a31
Mayank Chauhan <2003mayankchauhan@gmail.com>
2024-04-10 17:02:38 +0530

    commit

--- src/helper/apiUrl.js
+++ src/helper/apiUrl.js
@@ -1,3 +1,4 @@
-const API_BASE="http://localhost:4000";
+// const API_BASE="http://localhost:4000";
 // const API_BASE="https://xpod-backend.vercel.app";
+const API_BASE="https://smm-backend-1.vercel.app";
 export default API_BASE;
